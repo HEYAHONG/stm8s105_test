dbd3e757b31492111991c538a28e73695a372e4c
何亚红 <2229388563@qq.com>
2018-09-23 08:53:19 +0800

    initial,need SDCC and stm8flash

---
+++ Makefile
@@ -0,0 +1,48 @@
+ifndef PROJECT
+PROJECT=test
+endif
+
+SDCC=sdcc
+PACKIHX=packihx
+MAKEBIN=makebin
+
+#select stm8s103
+CFLAGS+=  -DSTM8S103 -mstm8  -DUSE_STDPERIPH_DRIVER  
+
+#use std_periph_driver
+CFLAGS+= -I./STM8S_StdPeriph_Lib/Libraries/STM8S_StdPeriph_Driver/inc 
+CFLAGS+= -I./
+CFLAGS+= -I./STM8S_StdPeriph_Lib/Libraries/STM8S_StdPeriph_Driver/src
+#use compiled lib
+CFLAGS+= -L./STM8S_StdPeriph_Lib/Libraries/STM8S_StdPeriph_Driver/lib -lSTM8S103.lib
+
+
+SRC=$(wildcard *.asm) $(wildcard *.c) 
+
+OBJ1_SRC=$(patsubst %.c,%.rel,$(SRC))
+OBJ_SRC=$(patsubst %.asm,%.rel,$(OBJ1_SRC))
+
+ASM_DST=$(patsubst %.rel,%.asm,$(OBJ_SRC))
+LST_DST=$(patsubst %.rel,%.lst,$(OBJ_SRC))
+RST_DST=$(patsubst %.rel,%.rst,$(OBJ_SRC))
+SYM_DST=$(patsubst %.rel,%.sym,$(OBJ_SRC))
+
+
+
+all: $(PROJECT).hex 
+
+
+$(PROJECT).hex:$(PROJECT).ihx
+	$(PACKIHX) $^ > $@ 
+
+
+$(PROJECT).ihx:$(OBJ_SRC)
+	$(SDCC) $(CFLAGS)  $^ -o $(PROJECT).ihx
+
+%.rel:%.c
+	$(SDCC) -c $(CFLAGS) $^ -o $@
+%.rel:%.asm
+	$(SDCC) -c $(CFLAGS) $^ -o $@
+clean:
+	-rm -rf $(OBJ_SRC) $(ASM_DST) $(LST_DST) $(RST_DST) $(SYM_DST)
+	-rm -rf $(PROJECT).ihx $(PROJECT).hex $(PROJECT).lk $(PROJECT).mem  $(PROJECT).map
---
+++ main.c
@@ -0,0 +1,83 @@
+/**
+  ******************************************************************************
+  * @file    Project/main.c 
+  * @author  MCD Application Team
+  * @version V2.3.0
+  * @date    16-June-2017
+  * @brief   Main program body
+   ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm8s.h"
+
+/* Private defines -----------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+#define LED_GPIO_PORT  (GPIOB)
+#define LED_GPIO_PINS  (GPIO_PIN_5)
+void Delay(uint16_t nCount)
+{
+  /* Decrement nCount value */
+  while (nCount != 0)
+  {   
+    nCount--;
+  }
+}
+
+void main(void)
+{
+/* Initialize I/Os in Output Mode */
+  GPIO_Init(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS, GPIO_MODE_OUT_PP_LOW_FAST);
+
+  /* Infinite loop */
+  while (1)
+  {
+  /* Toggles LEDs */
+    GPIO_WriteReverse(LED_GPIO_PORT, (GPIO_Pin_TypeDef)LED_GPIO_PINS);
+    Delay(0xffff);
+  }
+  
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval : None
+  */
+void assert_failed(u8* file, u32 line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
