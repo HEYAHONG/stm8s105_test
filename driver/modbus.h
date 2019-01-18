2348bb50ffd038dbe700efc2f628654fbb055787
HEYAHONG <2229388563@qq.com>
2019-01-18 10:15:34 +0800

    增加modbus缓冲区

--- driver/modbus.h
+++ driver/modbus.h
@@ -6,7 +6,7 @@ u16 CRC16(u8 *arr_buff,u8 len);
 //状态标志，0～7  表示下一个接收的数据在接收缓冲的位置.超过7表示其它含义（如接收完成）
 extern u8  modbus_status;
 //接收发送缓冲数组,只实现03和06功能，代码接收和发送均是8字节（加校验位）
-extern u8 modbus_buff[8];
+extern u8 modbus_buff[];
 
 void modbus_process(); //处理modbus
 // 不进行CRC校验，用于调试。不用可注释
